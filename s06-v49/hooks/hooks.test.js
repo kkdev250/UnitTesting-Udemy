58f62a69b66167bf1f013e208f9e0aa76ec7420f
Karol Kopicera <karol.kopicera@softwarehut.com>
2023-02-18 22:46:17 +0100

    after 56. Concurrent Tests

--- s06-v49/hooks/hooks.test.js
+++ s06-v49/hooks/hooks.test.js
@@ -20,30 +20,34 @@ afterEach(() => {
 afterAll(() => {
   console.log('afterAll()');
 });
+//if using hooks above in 'decribe' suite - they will apply only to tests inside the 'describe'
 
-it('should update the email', () => {
+it.concurrent('should update the email', () => {
   const newTestEmail = 'test2@test.com';
   user.updateEmail(newTestEmail);
 
   expect(user.email).toBe(newTestEmail);
 });
 
-it('should have an email property', () => {
+it.concurrent('should have an email property', () => {
   expect(user).toHaveProperty('email');
 });
 
-it('should store the provided email value', () => {
+it.concurrent('should store the provided email value', () => {
   expect(user.email).toBe(testEmail);
 });
 
-it('should clear the email', () => {
+it.concurrent('should clear the email', () => {
   user.clearEmail();
 
   expect(user.email).toBe('');
 });
 
-it('should still have an email property after clearing the email', () => {
+it.concurrent('should still have an email property after clearing the email', () => {
   user.clearEmail();
 
   expect(user).toHaveProperty('email');
 });
+
+//or... 
+//describe.concurrent(' - then all tests inside will be run concurrently - no need to use it.concurrent inside the 'decribe'
